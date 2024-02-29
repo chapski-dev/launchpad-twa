a0ee9e4f6d068b0ce386925085e413535d656970
ch1cano <ch1canoo358@gmail.com>
2024-02-29 16:06:40 +0300

    feat/2-v.2: add component ProfileBlock

--- src/assets/style/theme.ts
+++ src/assets/style/theme.ts
@@ -9,5 +9,11 @@ export const theme: DefaultTheme = {
     btn: `var(--tg-theme-button-color)`,
     btnText: `var(--tg-theme-button-text-color)`,
     bgSecondary: `var(--tg-theme-secondary-bg-color)`,
+    success: '#62c56d',
+    warning: '#FBBC05',
+  },
+  gradient: {
+    g1: 'linear-gradient(312deg, #82d3ff 0.46%, #ffa1ec 100%)',
+    g2: 'linear-gradient(45deg, #fb73e0 0%, #0092e0 100%)',
   },
 }
---
+++ src/features/Layout/components/Header/components/ProfileBlock/ProfileBlock.tsx
@@ -0,0 +1,33 @@
+import { FC, useState } from 'react'
+import { useTelegram } from 'hooks/useTelegram/useTelegram'
+import { SvgRequired, SvgVerified } from 'ui/icons'
+import * as S from './style'
+
+export const ProfileBlock: FC = () => {
+  const { user } = useTelegram()
+
+  const [statusVerified, setstatusVerified] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.ProfileBox>
+        <S.BgAvatar>
+          <S.ProfileName>{user?.first_name[0]}</S.ProfileName>
+        </S.BgAvatar>
+        <S.ConfirmationIcon>
+          <S.IconStatus>
+            {statusVerified ? <SvgVerified /> : <SvgRequired />}
+          </S.IconStatus>
+        </S.ConfirmationIcon>
+      </S.ProfileBox>
+      <S.InfoBlock>
+        <S.Name>{user?.first_name}</S.Name>
+        {!statusVerified ? (
+          <S.Pending>Verification required</S.Pending>
+        ) : (
+          <S.Verified>Account verified</S.Verified>
+        )}
+      </S.InfoBlock>
+    </S.Wrapper>
+  )
+}
