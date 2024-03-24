387526bd30d718b50bdd9e813ad83705852d78fb
Maks Zhers <maks.zhers@gmail.com>
2024-03-24 14:19:17 +0300

    fix: fix logic for buy tokens by TON

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,60 +1,56 @@
-import { FC, useCallback, useEffect, useMemo, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { useTonAddress, useTonConnectUI } from '@tonconnect/ui-react';
+import { FC, useCallback, useEffect, useMemo, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { useTonAddress } from '@tonconnect/ui-react'
 // import dayjs from 'dayjs'
 
-import { SaleV1FunC } from '@xton/user-sdk';
-
-import { Inter } from 'next/font/google';
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { Address } from 'ton-core';
-import { getICOProjectById } from 'api';
-import { AppRoutes } from 'constants/app';
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { getICOProjectById } from 'api'
+import { AppRoutes } from 'constants/app'
 import {
   ProjectInfoHeader,
   // ParticipateModal,
-} from 'domains/Project/components';
-import * as S from 'domains/Project/style';
-import { BackButton } from 'features/BackButton';
-import { Layout } from 'features/Layout/Layout';
+} from 'domains/Project/components'
+import * as S from 'domains/Project/style'
+import { BackButton } from 'features/BackButton'
+import { Layout } from 'features/Layout/Layout'
 // import { MainButton } from 'features/MainButton'
 // import { useSendTransaction } from 'hooks/useSendTransaction/useSendTransaction'
-import { MainButton } from 'features/MainButton';
-import { useProfileContext } from 'hooks/useProfileContext/useProfileContext';
-import { useSendTransaction } from 'hooks/useSendTransaction/useSendTransaction';
-import { useTelegram } from 'hooks/useTelegram/useTelegram';
-import { BuyPopup } from 'popups/BuyPopup/BuyPopup';
-import { VerificationPopup } from 'popups/VerificationPopup/VerificationPopup';
-import { Button } from 'ui/Button/Button';
-import { Container } from 'ui/Container/Container';
-import { Line } from 'ui/Line/Line';
-import { Loader } from 'ui/Loader/Loader';
+import { MainButton } from 'features/MainButton'
+import { useProfileContext } from 'hooks/useProfileContext/useProfileContext'
 
-const inter = Inter({ subsets: ['latin'] });
+import { useTelegram } from 'hooks/useTelegram/useTelegram'
+import { BuyPopup } from 'popups/BuyPopup/BuyPopup'
+// import { VerificationPopup } from 'popups/VerificationPopup/VerificationPopup'
+import { Button } from 'ui/Button/Button'
+import { Container } from 'ui/Container/Container'
+import { Line } from 'ui/Line/Line'
+import { Loader } from 'ui/Loader/Loader'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const Project: FC = () => {
-  const [isParticipateModalOpen, setIsParticipateModalOpen] = useState(false);
-  const [verificationPopupOpen, setVerificationPopupOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [isParticipateModalOpen, setIsParticipateModalOpen] = useState(false)
+  // const [verificationPopupOpen, setVerificationPopupOpen] = useState(false)
 
-  const router = useRouter();
+  const router = useRouter()
 
-  const userWalletAddress = useTonAddress();
+  const userWalletAddress = useTonAddress()
 
   // const [tonConnectUI] = useTonConnectUI()
 
   // const { sendTransaction } = useSendTransaction()
 
-  const { xapiProfileInfo } = useProfileContext();
+  const { xapiProfileInfo } = useProfileContext()
 
-  const { webApp } = useTelegram();
+  const { webApp } = useTelegram()
 
-  const { id } = router.query;
+  const { id } = router.query
 
   useEffect(() => {
-    webApp?.expand();
-  }, [webApp]);
+    webApp?.expand()
+  }, [webApp])
 
   const {
     data: project,
@@ -67,33 +63,33 @@ const Project: FC = () => {
     select: useCallback((data: any) => {
       const distributions: any[] =
         data.tokenomics.find(({ name }: any) => name === 'distribution')
-          ?.value || [];
+          ?.value || []
 
       const icoParams = data.tokenomics.find(
         ({ name }: any) => name === 'ico'
-      )?.value;
+      )?.value
 
       const icoFundDistributions =
         data.tokenomics.find(({ name }: any) => name === 'icoDistribution')
-          ?.value || [];
+          ?.value || []
 
       const getTotalSupply = () => {
         if (distributions) {
           const totalByDistributions = distributions.reduce<number>(
             (acc, curr) => Number(acc) + Number(curr.value),
             0
-          );
+          )
 
           if (icoParams) {
             const totalSupply =
-              totalByDistributions + Number(icoParams.jettonsAmount);
+              totalByDistributions + Number(icoParams.jettonsAmount)
 
-            return totalSupply;
+            return totalSupply
           }
 
-          return totalByDistributions;
+          return totalByDistributions
         }
-      };
+      }
 
       return {
         ...data,
@@ -104,9 +100,9 @@ const Project: FC = () => {
           : null,
         distributions,
         icoFundDistributions,
-      };
+      }
     }, []),
-  });
+  })
 
   // const {
   //   data: participantState,
@@ -127,90 +123,18 @@ const Project: FC = () => {
   //   }
   // )
 
-  const [TonConnectUI] = useTonConnectUI();
-  const { sendTransaction } = useSendTransaction();
-
-  const handleBuy = async () => {
-    /** 
-     * First part of flow (this is only for TON):
-    0. User clicks buy
-    1. You call queryWhitelist, get response
-    2. (выдает ошибку, принято решение заменить на createUserMessage) You call createUser, give whitelistpayload from previous one, give tonconnect, give sale address (give your own address, this is mock), some amount "0.01"
-    2. createUserMessage
-    3. buyUserMessage - they give you ConnectMessage, you can send them using tonconnect
-    4. accept transaction in ton wallet
-    5. get boc, call check transaction every 5 seconds to check results
-    --- If success, go to 6
-    6. getUserSaleAddress
-    7. call buyUser and amount
-    8. wait for user to confirm
-    9. wait for tx
-    10. getUserSaleStatus (mock routes class)
-     * */
-
-    try {
-      setLoading(true);
-      // Step 1:
-      const whitelist = await SaleV1FunC.queryWhitelist(Address.parse(userWalletAddress));
-      //! Step 2: (здесь как правило дропается ошибка на невалидный адрес, поэтому вроде как заменяем на createUserMessage)
-      // await SaleV1FunC.createUser(TonConnectUI, Address.parse(userWalletAddress), '0.01', whitelist);
-      // Step 2:
-      await SaleV1FunC.createUserMessage(Address.parse(userWalletAddress), '0.01', whitelist);
-      // Step 3:
-      const buyUserMessage = await SaleV1FunC.buyUserMessage(Address.parse(userWalletAddress), BigInt(9007199254740991));
-      // Step 4:
-      const { boc } = await sendTransaction(buyUserMessage);
-      
-      let currentAttempts = 0;
-      let success = false;
-
-      // Step 5: Функция для периодической проверки статуса транзакции
-      const checkTransactionStatus = async () => {
-        try {
-          if (currentAttempts >= 5) {
-            throw new Error('Exceeded maximum number of attempts to check your transaction.');
-          }
-
-          success = await SaleV1FunC.checkTransaction(boc);
-          if (!success) {
-            currentAttempts++;
-            setTimeout(checkTransactionStatus, 5000); // Повторно проверить через 5 секунд
-          }
-        } catch (error) {
-          setLoading(false);
-          alert(error);
-        }
-      };
-
-      if (boc) {
-        // Начать периодическую проверку статуса транзакции
-        setTimeout(checkTransactionStatus, 5000);
-      }
-
-      if (success) {
-        // Step 6 //!(проверить saleAddress: Address, userAddress: Address в методе getUserSaleAddress): 
-        await SaleV1FunC.getUserSaleAddress(Address.parse(userWalletAddress), Address.parse(userWalletAddress));
-        // Step 7: 
-        await SaleV1FunC.buyUser(TonConnectUI,Address.parse(userWalletAddress), BigInt(0.1))
-      }
-    } catch (error) {
-      alert(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   const toggleParticipateModal = () => {
     switch (xapiProfileInfo?.state) {
       case 'verified':
-        setIsParticipateModalOpen((prev) => !prev);
-        break;
+        setIsParticipateModalOpen((prev) => !prev)
+        break
 
       case 'unverified':
-        setVerificationPopupOpen((prev) => !prev);
-        break;
+        //TODO: temporary, delete after all tests
+        setIsParticipateModalOpen((prev) => !prev)
+        break
     }
-  };
+  }
 
   // const handleMainButtonClick = useCallback(() => {
   //   if (!userWalletAddress) {
@@ -244,18 +168,16 @@ const Project: FC = () => {
 
   const isLoading = useMemo(() => {
     if (userWalletAddress) {
-      return (
-        isProjectLoading
-      );
+      return isProjectLoading
     }
 
-    return isProjectLoading;
+    return isProjectLoading
   }, [
     // isCurrentIcoInfoLoading,
     // isParticipantStateLoading,
     isProjectLoading,
     userWalletAddress,
-  ]);
+  ])
 
   // const isEarlyClaimButtonDisplayed = useMemo(() => {
   //   if (!participantState || !participantState?.participated) {
@@ -286,7 +208,6 @@ const Project: FC = () => {
   //     alert('Early claim successfully submitted')
   //   }
   // }, [project, participantState, sendTransaction])
-
 
   return (
     <>
@@ -320,7 +241,7 @@ const Project: FC = () => {
                     )} */}
                       <Button
                         onClick={() => {
-                          router.push(AppRoutes.VestingDistribution);
+                          router.push(AppRoutes.VestingDistribution)
                         }}
                       >
                         Vesting Distiribution page
@@ -346,14 +267,15 @@ const Project: FC = () => {
                             : undefined
                         }
                       />
-                      <VerificationPopup
+
+                      {/* <VerificationPopup
                         onClose={toggleParticipateModal}
                         open={verificationPopupOpen}
-                      />
+                      /> */}
+
                       {!isParticipateModalOpen && (
                         <MainButton
-                          onClick={handleBuy}
-                          progress={loading}
+                          onClick={toggleParticipateModal}
                           text={
                             project.name === 'NebulaNet'
                               ? 'Join Waitlist'
@@ -401,7 +323,7 @@ const Project: FC = () => {
         )}
       </main>
     </>
-  );
-};
+  )
+}
 
-export default Project;
+export default Project
--- src/popups/BuyPopup/BuyPopup.tsx
+++ src/popups/BuyPopup/BuyPopup.tsx
@@ -1,5 +1,9 @@
 import { FC, useMemo, useState } from 'react'
+import { useTonAddress } from '@tonconnect/ui-react'
+import { SaleV1FunC } from '@xton/user-sdk'
+import { Address } from 'ton-core'
 import { MainButton } from 'features/MainButton'
+import { useSendTransaction } from 'hooks/useSendTransaction/useSendTransaction'
 import { Modal } from 'ui/Modal/Modal'
 import {
   WaitingForApproval,
@@ -22,6 +26,14 @@ export const BuyPopup: FC<BuyPopupProps> = (props) => {
 
   const [currentStatus, setCurrentStatus] = useState<BuyStatus>(status || 'buy')
 
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const userWalletAddress = useTonAddress()
+
+  const { sendTransaction } = useSendTransaction()
+
+  // const [tonConnectUI] = useTonConnectUI()
+
   const currentBuyPopupState = useMemo(() => {
     switch (currentStatus) {
       case 'buy':
@@ -39,21 +51,156 @@ export const BuyPopup: FC<BuyPopupProps> = (props) => {
     }
   }, [currentStatus])
 
-  const handleClick = () => {
-    switch (currentStatus) {
-      case 'buy':
+  // const handleClick = () => {
+  //   switch (currentStatus) {
+  //     case 'buy':
+  //       setCurrentStatus('loader')
+  //       break
+  //     case 'loader':
+  //       setCurrentStatus('waiting')
+  //       break
+  //     case 'waiting':
+  //       setCurrentStatus('success')
+  //       break
+  //     case 'join_waitlist':
+  //       alert('You have been successfully added to the waiting list.')
+  //       onClose(false)
+  //       break
+  //   }
+  // }
+
+  const handleBuy = async () => {
+    /** 
+     * First part of flow (this is only for TON):
+    0. User clicks buy
+    1. You call queryWhitelist, get response
+    2. (выдает ошибку, принято решение заменить на createUserMessage) You call createUser, give whitelistpayload from previous one, give tonconnect, give sale address (give your own address, this is mock), some amount "0.01"
+    2. createUserMessage
+    3. accept transaction in ton wallet
+    4. get boc, call check transaction every 5 seconds to check results
+    
+    5. getUserSaleAddress
+    6. call buyUser and amount
+    7. wait for user to confirm
+    8. wait for tx
+
+    ??10. getUserSaleStatus (mock routes class)
+     * */
+
+    try {
+      setIsLoading(true)
+      // Step 1:
+      const whitelist = await SaleV1FunC.queryWhitelist(
+        Address.parse(userWalletAddress)
+      )
+
+      console.log(whitelist)
+      // Step 2
+      const createUserMessage = await SaleV1FunC.createUserMessage(
+        Address.parse(userWalletAddress),
+        '0.01',
+        whitelist
+      )
+
+      // Step: 3
+      const { boc: createUserBoc } = await sendTransaction(createUserMessage)
+
+      // Step: 4
+      if (createUserBoc) {
         setCurrentStatus('loader')
-        break
-      case 'loader':
-        setCurrentStatus('waiting')
-        break
-      case 'waiting':
-        setCurrentStatus('success')
-        break
-      case 'join_waitlist':
-        alert('You have been successfully added to the waiting list.')
-        onClose(false)
-        break
+
+        let currentAttempts = 0
+        let isCreateUserTrxSigned = false
+
+        const checkTransactionStatus = async () => {
+          try {
+            if (currentAttempts >= 5) {
+              throw new Error(
+                'Exceeded maximum number of attempts to check your transaction.'
+              )
+            }
+
+            isCreateUserTrxSigned = await SaleV1FunC.checkTransaction(
+              createUserBoc
+            )
+
+            if (!isCreateUserTrxSigned) {
+              currentAttempts++
+              setTimeout(checkTransactionStatus, 5000) // Повторно проверить через 5 секунд
+
+              return
+            }
+
+            //Step 5  (if trx status true)
+            await SaleV1FunC.getUserSaleAddress(
+              Address.parse(userWalletAddress),
+              Address.parse(userWalletAddress)
+            )
+
+            //Step 6
+            const buyUserMessage = await SaleV1FunC.buyUserMessage(
+              Address.parse(userWalletAddress),
+              BigInt(10000000)
+            )
+
+            //Step 7
+            const { boc: buyUserBoc } = await sendTransaction(buyUserMessage)
+
+            //Step 8
+            if (buyUserBoc) {
+              let currentAttempts = 0
+              let isBuyUserTrxSigned = false
+
+              const checkBuyUserTrx = async () => {
+                try {
+                  if (currentAttempts >= 5) {
+                    throw new Error(
+                      'Exceeded maximum number of attempts to check your transaction.'
+                    )
+                  }
+
+                  isBuyUserTrxSigned = await SaleV1FunC.checkTransaction(
+                    buyUserBoc
+                  )
+
+                  if (!isBuyUserTrxSigned) {
+                    currentAttempts++
+                    setTimeout(checkBuyUserTrx, 5000) // Повторно проверить через 5 секунд
+
+                    return
+                  }
+
+                  setCurrentStatus('success')
+                  setIsLoading(false)
+                } catch (error) {
+                  setIsLoading(false)
+                  alert(error)
+                }
+              }
+
+              checkBuyUserTrx()
+
+              if (isBuyUserTrxSigned) {
+                setCurrentStatus('success')
+              }
+            }
+
+            return
+          } catch (error) {
+            setIsLoading(false)
+            console.log(error)
+            alert(error)
+
+            return
+          }
+        }
+
+        checkTransactionStatus()
+      }
+    } catch (error) {
+      alert(error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -70,9 +217,10 @@ export const BuyPopup: FC<BuyPopupProps> = (props) => {
       title="Buy XTON"
     >
       {currentBuyPopupState}
-      {currentStatus !== 'success' && (
+      {currentStatus !== 'success' && currentStatus !== 'loader' && (
         <MainButton
-          onClick={handleClick}
+          onClick={handleBuy}
+          progress={isLoading}
           text={
             currentStatus === 'buy'
               ? 'Buy XTON'
